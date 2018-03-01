e65516cb811b72faff95e9f59716d329f16add3a
Thomas Xing <thomas.xing@hirede.com>
2018-03-01 17:45:37 +0800

    Init:Thomas-初始化项目。

---
+++ Baza.Experiment/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Baza.Experiment.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace Baza.Experiment.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return Content("Hell world");
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult DateTimeTest([FromBody]DateTimeRequest request)
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"原始数据:{request.Date}");
+            sb.AppendLine($"UTC数据:{request.Date.ToUniversalTime()}");
+            sb.AppendLine($"UTC String数据:{request.Date.ToUniversalTime().ToString("O")}");
+
+            return Content(sb.ToString());
+        }
+    }
+}
---
+++ Baza.Experiment/Models/DateTimeRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Baza.Experiment.Models
+{
+    public class DateTimeRequest
+    {
+        public string Name { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
